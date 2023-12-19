c1d2a2f53a8451d401ffff0b68e4278f3326d966
Sylvan LE DEUNFF <sledeunf@gmail.com>
2023-12-19 02:03:11 +0100

    Add CORS middleware

--- cookbook/api/factory.py
+++ cookbook/api/factory.py
@@ -1,18 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from cookbook.core.config import Config
-from cookbook.api.metadata import API_DESCRIPTION, API_TAGS, API_TITLE, API_VERSION
 
 import cookbook.api.healthcheck
 import cookbook.api.v1.ingredients
 import cookbook.api.v1.recipes
+from cookbook.api.metadata import API_DESCRIPTION, API_TAGS, API_TITLE, API_VERSION
+from cookbook.core.config import Config
 
 
 def create_api(config: Config):
     app = FastAPI(
         title=API_TITLE, version=API_VERSION, description=API_DESCRIPTION, openapi_tags=API_TAGS, docs_url="/"
     )
-    app.add_middleware(CORSMiddleware, allow_origins = "*", allow_methods = "*", allow_headers = "*", allow_credentials = True)
+    app.add_middleware(CORSMiddleware, allow_origins="*", allow_methods="*", allow_headers="*", allow_credentials=True)
 
     app.include_router(cookbook.api.healthcheck.router)
     app.include_router(cookbook.api.v1.ingredients.router, tags=["ingredients"], prefix="/v1")
